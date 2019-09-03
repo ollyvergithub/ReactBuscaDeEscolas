0f9565b35bf9c4ef05e1f7d6ed2ebde6438b97a5
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-09-03 14:16:02 -0300

    Só Refatorações

--- busca-de-escolas/src/Componentes/FormularioBuscaDeEscolas.js
+++ busca-de-escolas/src/Componentes/FormularioBuscaDeEscolas.js
@@ -6,6 +6,7 @@ import SelectCustomizado from './SelectCustomizado';
 import AutocompleteComDownshift from './AutocompleteComDownshift';
 import Paginacao from './Paginacao'
 
+
 class FormularioBuscaDeEscolas extends React.Component{
 
     constructor(){
@@ -126,33 +127,20 @@ class FormularioBuscaDeEscolas extends React.Component{
                                 <AutocompleteComDownshift  escolas_autocomplete = {this.state.escolas_autocomplete} onInputChangeDownshift = {this.onInputChangeDownshift.bind(this)} downshiftOnChange = {this.downshiftOnChange.bind(this)} />
                             </article>
 
-
                             <article className="form-group col-md-4">
-
                                 <SelectCustomizado campoApiValue="tipoesc" retornoApi={this.state.tipoEscola} value={this.state.tipoEscolaSelect} onChange ={this.escoheTipoEscola.bind(this)} className="form-control" id="busca_tipo_de_escola" name="busca_tipo_de_escola" label="Escolha um Tipo de Escola"/>
-
                             </article>
 
                             <article className="form-group col-md-4">
-                                {/*Os parametros campoApiValue e campoApiExibicao, são os campos retornados pela API*/}
                                 <SelectCustomizado campoApiValue="dre" campoApiExibicao='diretoria' retornoApi={this.state.dres} value={this.state.dreSelect} onChange ={this.escolheDre.bind(this)} className="form-control" id="busca_dre" name="busca_dre" label="Escolha uma DRE"/>
-
                             </article>
-
-                            <button id="form_submit" name="form_submit" className="btn btn-primary" type="submit">Buscar
-                                Escolas
-                            </button>
+                            <button id="form_submit" name="form_submit" className="btn btn-primary" type="submit">Buscar Escolas </button>
                         </section>
                     </fieldset>
                 </form>
 
                 {
-                    this.state.listaDeEscolasRetornadasPelaBusca.length > 0 ? (
-                        <ExibeEscolasRetornadasPelaBusca listaDeEscolasRetornadasPelaBusca = {this.state.listaDeEscolasRetornadasPelaBusca}/>
-                    ): (
-                        null
-                    )
-                }
+                    this.state.listaDeEscolasRetornadasPelaBusca.length > 0 ? (<ExibeEscolasRetornadasPelaBusca listaDeEscolasRetornadasPelaBusca = {this.state.listaDeEscolasRetornadasPelaBusca}/>): ( null ) }
 
                 {
                     this.state.PaginacaoPaginasTotal > 0 ? (<Paginacao buscaEscolasPaginacao = {this.buscaEscolasPaginacao.bind(this)} PaginacaoPaginasTotal = {this.state.PaginacaoPaginasTotal}/>) : (null)
